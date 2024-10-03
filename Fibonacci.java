ea4536f87570b814e8aeab0e03b7ffa30692f35b
Muriiloedu <161713469+Muriiloedu@users.noreply.github.com>
2024-10-03 08:49:21 -0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FibonacciChecker {
+public class Fibonacci {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
