9f27a09f25c6031a9a9fa88a3c9ea86e5a71b83f
Muriiloedu <161713469+Muriiloedu@users.noreply.github.com>
2024-10-03 08:47:44 -0300

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class FibonacciChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Entrada do número a ser verificado
+        System.out.println("Informe um número:");
+        int number = scanner.nextInt();
+        
+        // Verifica se o número pertence à sequência de Fibonacci
+        if (isFibonacci(number)) {
+            System.out.println("O número " + number + " pertence à sequência de Fibonacci.");
+        } else {
+            System.out.println("O número " + number + " NÃO pertence à sequência de Fibonacci.");
+        }
+        
+        scanner.close();
+    }
+
+    public static boolean isFibonacci(int num) {
+        int a = 0, b = 1, c = 0;
+        
+        if (num == 0 || num == 1) {
+            return true;
+        }
+
+        while (c < num) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return c == num;
+    }
+}
